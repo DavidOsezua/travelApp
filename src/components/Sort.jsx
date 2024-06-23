14702ccb48e231b865f1d71c1bbeeaef1de17eb8
DavidOsezua <102161226+DavidOsezua@users.noreply.github.com>
2024-06-23 05:03:22 +0100

    added the sort component

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Sort = () => {
   return (
-    <div>
-      <div>
+    <div className="flex justify-between items-center mt-[2rem] text-[0.8rem]">
+      <div className={`text-[#fff]`}>
         <p>300+ properties</p>
 
         <div>
@@ -13,7 +13,7 @@ const Sort = () => {
         </div>
       </div>
 
-      <select>
+      <select className="bg-transparent text-[#fff]  p-[1rem] rounded-md outline outline-1 outline-[#fff]">
         <option>Sort by Recommended</option>
       </select>
     </div>
